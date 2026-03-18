29d0537428c1f139e726b4add8b5925b7d686e2d
takumi-uezono0 <uezono1998@gmail.com>
2026-03-18 22:33:40 +0900

    コピーボタンの追加

--- script.js
+++ script.js
@@ -90,3 +90,20 @@ function updateMonthly(hours) {
 
   return total;
 }
+
+//コピーボタン
+document.addEventListener("DOMContentLoaded", function () {
+  function copyClipboard(event) {
+    const clipboard = document.createElement("textarea");
+    clipboard.value = event.target.previousElementSibling.value;
+    event.target.appendChild(clipboard);
+    clipboard.select();
+    document.execCommand("copy");
+    event.target.removeChild(clipboard);
+  }
+
+  const buttons = document.getElementsByClassName("copy_clipboard");
+  for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", copyClipboard);
+  }
+});
